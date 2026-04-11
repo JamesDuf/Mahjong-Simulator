9ae0b44b539b009e944dda6cfb5505cda5c7055b
JamesDuf <139725130+JamesDuf@users.noreply.github.com>
2026-04-11 19:06:56 -0400

    implemented game state driver

--- game_state.py
+++ game_state.py
@@ -2,6 +2,7 @@ from player import Player
 from wall import Wall
 from discard_pile import DiscardPile
 from tiles import Tile, Suit, HONOR_TILES, WIND_TILES
+from hand import Hand
 
 class GameState:
     """Round-level state manager for the 4-player game."""
@@ -55,6 +56,43 @@ class GameState:
 
         # reset player hands
         for player in self.players: 
-            player.hand = player.hand.__class__()
+            player.hand = Hand()
 
-        # deal 13 
+        # deal 13 tiles to each player
+        #Each player picks up 13 tiles in a row, one after the other
+        for _ in range(13):
+            for player in self.players:
+                player.draw_tile(self.wall.draw())
+            
+        self.round_started = True
+
+    def draw_for_current_player(self):
+        if not self.round_started:
+            raise RuntimeError("Round has not yet started.")
+        if self.wall.is_empty():
+            raise RuntimeError("Cannot draw: wall empty.")
+        
+        drawn_tile = self.wall.draw()
+        self.current_player.draw_tile(drawn_tile)
+        return drawn_tile 
+    
+    def discard_for_current_player(self, tile):
+        if not self.round_started:
+            raise RuntimeError("Round has not yet started.")
+        if not isinstance(tile, Tile):
+            raise TypeError("discard_tile_for_current_player expects a Tile")
+        
+        discarded_tile = self.current_player.discard_tile(tile)
+        self.discard_pile.add(discarded_tile)
+        return discarded_tile
+    
+    def next_turn(self) -> None:
+        self.current_player_idx = (self.current_player_idx + 1 ) % 4 #will always have index 0,1,2,3
+
+    def snapshot(self):
+        return {
+            "current_player": self.current_player.name, 
+            "remaining_wall_tiles": len(self.wall),
+            "discarded_amount": len(self.discard_pile),
+            "hand_sizes": {p.name: len(p.hand) for p in self.players}
+        }
